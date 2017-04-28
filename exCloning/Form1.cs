cb3130cea1fdd17d00ba616a11c30bf263b56436
hostage112 <ess.aleksandr@gmail.com>
2017-04-28 10:28:28 +0300

    current

--- exCloning/Form1.cs
+++ exCloning/Form1.cs
@@ -34,6 +34,8 @@ namespace exCloning
 
         private void btn_getSelected_Click(object sender, EventArgs e)
         {
+            txt_report.Text = "";
+
             try
             {
                 getSelected();
